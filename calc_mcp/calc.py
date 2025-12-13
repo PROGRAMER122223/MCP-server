7381d3bd87d34ae6d868bba9062fb5a1a132ce0f
programer122223 <pratibha.indel@gmail.com>
2025-12-13 11:48:39 -0500

    added resource and resource template

--- calc_mcp/calc.py
+++ calc_mcp/calc.py
@@ -55,6 +55,17 @@ def divide(a: int|float, b: int|float) -> float:
     """
     return a / b
 
+@mcp.resource(uri="data://operations")
+def operations() -> list[str]:
+    return["add","sub","mul", "div"]
+
+@mcp.resource(uri="data://operation/{intent}")
+def get_operation(intent:str)->str:
+    if intent == "add":
+        return "add"
+    else:
+        return " i don't know"
+
 
 if __name__ == "__main__":
     mcp.run(transport="stdio")
